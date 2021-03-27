6a6d35b8295ace72b7bb97b9f5ac6c7feb3b8ecd
maksim.maltsev@t-systems.com <maksim.maltsev@t-systems.com>
2021-03-27 16:15:29 +0300

    Monitoting with Graphana and

--- src/main/java/com/example/kafka_example/config/Configuration.java
+++ src/main/java/com/example/kafka_example/config/Configuration.java
@@ -1,6 +1,9 @@
 package com.example.kafka_example.config;
 
+import io.micrometer.core.instrument.MeterRegistry;
 import org.apache.kafka.clients.admin.NewTopic;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.actuate.autoconfigure.metrics.MeterRegistryCustomizer;
 import org.springframework.context.annotation.Bean;
 
 @org.springframework.context.annotation.Configuration
@@ -9,5 +12,11 @@ public class Configuration {
     @Bean
     public NewTopic initTopic(){
         return new NewTopic("user", 3, (short) 3);
+    }
+
+    @Bean
+    MeterRegistryCustomizer<MeterRegistry> configurer(
+            @Value("${spring.application.name}") String applicationName) {
+        return (registry) -> registry.config().commonTags("application", applicationName);
     }
 }
